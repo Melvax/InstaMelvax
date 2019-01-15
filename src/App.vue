f88c29f1f1b1b12e5e4a919663370d26a2e4867b
Melvax <melvin.adjei@gmail.com>
2019-01-15 17:31:21 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <!-- img width="25%" src="./assets/logo.png" -->
+    <!-- HelloWorld/ -->
+    <header />
+  </div>
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld";
+import Header from "./components/Header";
+
+export default {
+  name: "App",
+  components: {
+    HelloWorld,
+    Header
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
